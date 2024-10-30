a5e380361f9794ca7ada87ecec34e1f46295b3db
EMMANUELRR2005 <159630501+EMMANUELRR2005@users.noreply.github.com>
2024-10-29 23:32:26 -0600

    Initial commit

---
+++ app/src/main/java/gt/edu/umg/proyecto/Camara.java
@@ -0,0 +1,90 @@
+package gt.edu.umg.proyecto;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Camara extends AppCompatActivity {
+
+    Button bottonTomar;
+    ImageView imgView;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int PERMISSION_REQUEST_CAMERA = 100;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_camara);
+
+        bottonTomar = findViewById(R.id.bottonTomar);
+        imgView = findViewById(R.id.imgView);
+
+        bottonTomar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkCameraPermission();
+            }
+        });
+    }
+
+    private void checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.CAMERA},
+                    PERMISSION_REQUEST_CAMERA);
+        } else {
+            abrirCamara();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                abrirCamara();
+            } else {
+                Toast.makeText(this, "Se necesita permiso de cámara", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void abrirCamara(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            imgView.setVisibility(View.VISIBLE);
+            imgView.setImageBitmap(imgBitmap);
+        }
+    }
+}
+
---
+++ app/src/main/java/gt/edu/umg/proyecto/HomeActivity.java
@@ -0,0 +1,68 @@
+package gt.edu.umg.proyecto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import gt.edu.umg.proyecto.DBThings.DbContactos;
+
+public class HomeActivity extends AppCompatActivity {
+    EditText editTextUsuarioNombre, editTextTelefono, editTextCorreo;
+    Button buttonFoto, buttonGuardar;
+    DbContactos dbContactos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //Para iniciar textos y botones
+        editTextUsuarioNombre = findViewById(R.id.editTextUsuarioNombre);
+        editTextTelefono = findViewById(R.id.editTextTelefono);
+        editTextCorreo = findViewById(R.id.editTextCorreo);
+        buttonFoto = findViewById(R.id.buttonFoto);
+        buttonGuardar = findViewById(R.id.buttonGuardar);
+
+        //Para iniciar la BD
+        dbContactos = new DbContactos(this);
+
+        //Proceso para que el boton guardar, realice el guardado
+        buttonGuardar.setOnClickListener(v -> {
+            //En este proceso obtendra los valores asignados
+            String Nombre = editTextUsuarioNombre.getText().toString();
+            String Telefono = editTextTelefono.getText().toString();
+            String Correo = editTextCorreo.getText().toString();
+
+            //Para que se logren insertar los datos a la BD
+            long id = dbContactos.insertaContacto(Nombre, Telefono, Correo);
+            if (id > 0) {
+                Toast.makeText(this, "Guardado exitoso", Toast.LENGTH_SHORT).show();
+                //Para limpiar campos
+                editTextUsuarioNombre.setText("");
+                editTextTelefono.setText("");
+                editTextCorreo.setText("");
+            } else {
+                Toast.makeText(this, "Ha ocurrido un error :(", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Listener para el boton foto
+        buttonFoto.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, Camara.class);
+            startActivity(intent);});
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
